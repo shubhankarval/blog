17cce5a0f39a5468c265cdfb6739a3f76bbccfdd
Shubhankar <shubhankarvalimbe@gmail.com>
2026-03-28 09:17:26 -0400

    Update blog index layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   const posts = getAllPosts();
 
   return (
-    <div className="px-6 py-8 sm:px-10">
+    <div className="px-4 py-8 sm:px-6 lg:px-10">
       <h1 className="text-5xl font-bold">blog.</h1>
       <div className="mt-14 space-y-10 md:space-y-15">
         {posts.map((post) => (
