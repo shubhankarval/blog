fe6e4fea8d097f9191fbc0c29d2fa4a192db90d4
Shubhankar <shubhankarvalimbe@gmail.com>
2026-07-21 15:21:22 -0400

    Fix build

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -7,34 +7,37 @@ import Header from '@components/Header';
 export const dynamicParams = false;
 
 export function generateStaticParams() {
-  const posts = getAllPosts();
+  // const posts = getAllPosts();
 
-  return posts.map((post) => ({
-    slug: post.slug,
-  }));
+  // return posts.map((post) => ({
+  //   slug: post.slug,
+  // }));
+  return [];
 }
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
-  const slug = (await params).slug;
-  const { default: Blog } = await import(`@/blogs/${slug}.mdx`);
+  notFound();
+  // const slug = (await params).slug;
 
-  const frontmatter = getPostBySlug(slug);
-  if (!frontmatter) {
-    notFound();
-  }
+  // const frontmatter = getPostBySlug(slug);
+  // if (!frontmatter) {
+  //   notFound();
+  // }
 
-  return (
-    <div className="space-y-6">
-      <Header />
-      <div className="space-y-4.5">
-        <Meta
-          title={frontmatter.title}
-          time={frontmatter.readingTime}
-          date={frontmatter.updatedAt}
-          tags={frontmatter.tags}
-        />
-        <Blog />
-      </div>
-    </div>
-  );
+  // const { default: Blog } = await import(`@/blogs/${slug}.mdx`);
+
+  // return (
+  //   <div className="space-y-6">
+  //     <Header />
+  //     <div className="space-y-4.5">
+  //       <Meta
+  //         title={frontmatter.title}
+  //         time={frontmatter.readingTime}
+  //         date={frontmatter.updatedAt}
+  //         tags={frontmatter.tags}
+  //       />
+  //       <Blog />
+  //     </div>
+  //   </div>
+  // );
 }
